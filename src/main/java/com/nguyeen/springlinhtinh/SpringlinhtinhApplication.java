067ea6c60bf25fb8e5a96a2f82f42c91c39c9669
nguyeen <tranquocnguyeen@gmail.com>
2024-07-03 14:07:04 +0700

    init project

---
+++ src/main/java/com/nguyeen/springlinhtinh/SpringlinhtinhApplication.java
@@ -0,0 +1,13 @@
+package com.nguyeen.springlinhtinh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringlinhtinhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringlinhtinhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nguyeen/springlinhtinh/SpringlinhtinhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nguyeen.springlinhtinh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringlinhtinhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
